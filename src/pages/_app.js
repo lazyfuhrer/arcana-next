71ccfd105e8f1c1355e228fee78e9e7948a2830f
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-02-16 19:38:03 +0530

    setup auth

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+import { getAuth } from "../../auth/getArcanaAuth";
+import { ProvideAuth } from "../../auth/useArcanaAuth";
+
+const auth = getAuth();
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ProvideAuth provider={auth}>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useArcanaAuth } from "../../auth/useArcanaAuth";
+import Loader from "../components/loader";
+import { Info } from "../components/info";
+import styles from "./index.module.css";
+
+export default function IndexPage() {
+  const { user, connect, isLoggedIn, loading, loginWithSocial, provider } =
+    useArcanaAuth();
+  const onConnectClick = async () => {
+    try {
+      await connect();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const onConnect = () => {
+    console.log("connected");
+  };
+  React.useEffect(() => {
+    provider.on("connect", onConnect);
+    return () => {
+      provider.removeListener("connect", onConnect);
+    };
+  }, [provider]);
+  return (
+    <>
+      {loading && <Loader secondaryColor="#101010" strokeColor="#101010" />}
+      {!loading && !isLoggedIn && (
+        <button className={styles.Btn} onClick={onConnectClick}>
+          Connect
+        </button>
+      )}
+      {!loading && isLoggedIn && <Info info={user} />}
+    </>
+  );
+}
